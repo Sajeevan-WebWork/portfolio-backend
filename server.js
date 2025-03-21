64be636d50a914e4b5b590f8792bdc517a45b324
sajeevan Balasingam <sajeeevan.b@edrevel.com>
2025-03-21 14:16:58 +0530

    server file update

--- server.js
+++ server.js
@@ -80,6 +80,10 @@ app.post("/contact", async (req, res) => {
     }
 });
 
+app.get("/", (req, res) => {
+    res.send("<h1>API is Running successfully</h1>")
+})
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
